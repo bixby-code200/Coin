8c605b8d0146cd5494e9b1a1fddbe21a2ab5a95f
unknown <onelife0985@gmail.com>
2019-06-03 16:44:25 +0900

    coin kinds & price

--- code/showVCList.js
+++ code/showVCList.js
@@ -1,8 +1,24 @@
+function makeResult(data){
+  const resultArray = []
+  const timeStamp = data.timestamp;
+  const payCurrency = data.payment_currency;
+  delete data.timestamp
+  delete data.payment_currency;
+  for(let key in data){
+    let dum = {};
+    dum.name = String(key);
+    dum.value = data[key].bids[0].price;
+    resultArray.push(dum);
+  }
+  return resultArray
+}
+
+
 module.exports.function = function showVCList (virtualCoins) {
   
   const http = require('http');
   // 가상화폐 목록 및 가격 가져와서 보여줌.
   const base_url = 'https://api.bithumb.com/public/orderbook/ALL';
   const res = http.getUrl(base_url, {format:'json'});
-  return String(res.data)
+  return makeResult(res.data)
 }
